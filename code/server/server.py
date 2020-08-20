21722e4947c5fac5f0ab01b9cbd0c93dcff52cae
xiaoxiae <tomas.slama.131@gmail.com>
2020-08-20 12:07:35 +0200

    vylepšení server outliny

--- code/server/server.py
+++ code/server/server.py
@@ -1,18 +1,55 @@
 #!/usr/bin/env python3
 
-import socket
-import sys
+from threading import *
+from socket import *
+from typing import *
+import os
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-sock.bind(('localhost', 9106))
-sock.listen(1)
-
-while True:
-    print("waiting")
-    connection, client_address = sock.accept()
-
-    print(connection, client_address)
 
 class MyntServer:
-    """A class for serving the Mynt clients."""
+
+    groups: Dict[str, Thread] = {}
+
+    IP: str = "127.0.0.1"
+    PORT: int = 9106
+
+    ID_LENGTH = 16
+
+    @classmethod
+    def spawn_id_thread(cls):
+        """Spawn a thread that takes care of a single ID."""
+
+    @classmethod
+    def spawn_thread_group(cls):
+        """Spawn a thread that takes care of a group of IDs."""
+
+    def run(self):
+        """Run the server, watching for sockets and spawning thread groups."""
+        try:
+            s = socket()
+            s.bind((self.IP, self.PORT))
+        except socket.error as e:
+            print(str(e))
+
+        s.listen(5)
+
+        try:
+            while True:
+                client, address = s.accept()
+
+                # first read the ID (of a reasonable length)
+                id = client.recv(2048)
+
+                if id in self.groups:
+                    pass  # TODO: if a thread that handles this particular ID exists
+                else:
+                    pass  # TODO: spawn new thread that handles this ID
+        except KeyboardInterrupt:
+            pass
+
+        s.close()
+
+
+if __name__ == "__main__":
+    server = MyntServer()
+    server.run()
