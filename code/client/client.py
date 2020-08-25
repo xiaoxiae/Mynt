9ef2b3b3bd1520debcd793132c45a1739603f638
xiaoxiae <tomas.slama.131@gmail.com>
2020-08-25 18:39:39 +0200

    client testing code

--- code/client/client.py
+++ code/client/client.py
@@ -3,26 +3,32 @@ from socket import socket, AF_INET, SOCK_STREAM, SHUT_RDWR
 from time import sleep
 from uuid import getnode as get_mac
 
+MAX_MESSAGE_SIZE = 1024
+
+
 def send_data(data: str, sock: socket):
     sock.sendall(data.encode("utf-8") + b"\n")
 
+
 def receive_data(sock: socket):
     return sock.recv(MAX_MESSAGE_SIZE)
+
 
 if __name__ == "__main__":
     uid = hex(get_mac())
     mynt_id = "test"
 
-    SEND = True
+    SEND = False
 
     # first send some data
     sock = socket(AF_INET, SOCK_STREAM)
-    sock.connect(('localhost', 9106))
+    sock.connect(("localhost", 9106))
 
     if SEND:
         send_data(f"{uid} | {mynt_id} | test command", sock)
         sock.close()
     else:
         send_data(f"{uid}123 | {mynt_id}", sock)
-        print(sock.recv(1024))
+
+        print(receive_data(sock))
         sock.close()
