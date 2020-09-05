6a2627bcdb20f509e5c5997cc87634e2fb455d0d
xiaoxiae <tomas.slama.131@gmail.com>
2020-09-05 17:55:51 +0200

    add default Client class

--- code/client/client.py
+++ code/client/client.py
@@ -1,34 +1,50 @@
 """A module for communicating with the Mynt server."""
 from socket import socket, AF_INET, SOCK_STREAM, SHUT_RDWR
+from typing import *
 from time import sleep
 from uuid import getnode as get_mac
 
-MAX_MESSAGE_SIZE = 1024
+
+class Client:
+    """A class for talking to the Mynt server (as a client)."""
+
+    MAX_MESSAGE_SIZE = 1024  # TODO: read from some config file
+    ADDRESS = ("localhost", 9106)  # TODO: IP
+
+    def __init__(self, mynt_id: str, uid: Optional[str] = None):
+        self.mynt_id = mynt_id
+        self.uid = uid or hex(get_mac())
+
+    def connect(function):
+        """a decorator for connecting to the server using a socket, doing stuff and
+        then closing the socket."""
+
+        def wrapper(self, *args, **kwargs):
+            sock = socket(AF_INET, SOCK_STREAM)
+            sock.connect(self.ADDRESS)
+            result = function(self, sock, *args, **kwargs)
+            sock.close()
+            return result
+
+        return wrapper
+
+    @connect
+    def send(self, sock: socket, data: Optional[str] = None):
+        """A function for sending data to the Mynt server. If no data is specified, only
+        the UID and Mynt ID are sent, prompting the server to send a message back."""
+        sock.sendall(f"{self.uid} | {self.mynt_id} | {data}\n".encode("utf-8"))
+
+    @connect
+    def receive(self, sock: socket) -> str:
+        """A private function for receiving data from the Mynt server."""
+        sock.sendall(f"{self.uid} | {self.mynt_id}\n".encode("utf-8"))
+        return sock.recv(self.MAX_MESSAGE_SIZE).decode("utf-8")
 
 
-def send_data(data: str, sock: socket):
-    sock.sendall(data.encode("utf-8") + b"\n")
-
-
-def receive_data(sock: socket):
-    return sock.recv(MAX_MESSAGE_SIZE)
-
-
+# try to talk between the two clients (when the server is running)
 if __name__ == "__main__":
-    uid = hex(get_mac())
-    mynt_id = "test"
+    c1 = Client("m1", "c1")
+    c2 = Client("m1", "c2")
 
-    SEND = False
-
-    # first send some data
-    sock = socket(AF_INET, SOCK_STREAM)
-    sock.connect(("localhost", 9106))
-
-    if SEND:
-        send_data(f"{uid} | {mynt_id} | test command", sock)
-        sock.close()
-    else:
-        send_data(f"{uid}123 | {mynt_id}", sock)
-
-        print(receive_data(sock))
-        sock.close()
+    c1.send("test")
+    print(c2.receive())
