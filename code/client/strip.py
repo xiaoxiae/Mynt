56ad5ff5c542af693bf5fad340b102f7315bc9cd
xiaoxiae <tomas.slama.131@gmail.com>
2020-09-05 18:05:45 +0200

    strip: freq_hz is actually default

--- code/client/strip.py
+++ code/client/strip.py
@@ -6,10 +6,9 @@ import argparse
 class Strip:
     LED_COUNT = 5
     LED_PIN = 18
-    LED_FREQ_HZ = 2000  # from WS2813 datasheet
 
     def __init__(self):
-        self.strip = PixelStrip(self.LED_COUNT, self.LED_PIN, freq_hz=LED_FREQ_HZ)
+        self.strip = PixelStrip(self.LED_COUNT, self.LED_PIN)
         self.strip.begin()
 
     def set_color(self, i: int, color: int):
