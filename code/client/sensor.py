3c4da3cd3d553dafe2d9ba7160bcac8ed8523bcb
xiaoxiae <tomas.slama.131@gmail.com>
2020-09-08 14:06:49 +0200

    add default sensor code

--- code/client/sensor.py
+++ code/client/sensor.py
@@ -1,1 +1,25 @@
 """A module for handling sensor data."""
+import asyncio
+
+
+class Sensor:
+    """A class for working with the heartbeat sensor."""
+
+    checked: bool = False  # is toggled when a beat is registered via the check function
+    check_frequency: float = 50  # how frequently to check (per second)
+
+    def __init__(self):
+        pass  # TODO: init
+
+    def did_a_beat(self):
+        """Return True if the sensor detected a beat we haven't processed yet."""
+        result = self.checked
+        self.checked = False
+        return result
+
+    async def check(self):
+        while True:
+            await asyncio.sleep(1 / self.check_frequency)
+
+            if False:  # TODO: stuff with checking the sensor values
+                self.checked = True
