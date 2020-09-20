03fcad8d3357194bc59771c6f6111583e2e4483b
xiaoxiae <tomas.slama.131@gmail.com>
2020-09-20 11:34:27 +0200

    add connection via decorator back to client.py
    The code is a lot cleaner this way...

--- code/client/client.py
+++ code/client/client.py
@@ -1,61 +1,60 @@
 """A module for communicating with Mynt's server."""
+# disabled because Pylint doesn't like decorators that mess with parameters
+# pylint: disable=E1120
 import asyncio
 from typing import *
 from uuid import getnode as get_mac
 
+def connect_and_close(function):
+    """a decorator for connecting to the server using a socket, doing stuff and
+    then closing the socket."""
+
+    async def wrapper(self, *args, **kwargs):
+        if self.mynt_id is None:
+            return
+
+        reader, writer = await asyncio.open_connection(*self.ADDRESS)
+
+        result = await function(self, reader, writer, *args, **kwargs)
+
+        await writer.drain()
+        writer.close()
+        await writer.wait_closed()
+
+        return result
+
+    return wrapper
+
 
 class Client:
+
     MAX_MESSAGE_SIZE = 1024
     ADDRESS = ("localhost", 9106)  # TODO: IP
     SEPARATOR = " | "
 
     def __init__(self, uid: Optional[str] = None):
+        self.mynt_id = None
         self.uid = uid or hex(get_mac())
-        self.mynt_id is None
 
     def __join(self, *args) -> bytes:
         """Create a message by joining parts of it using a separator"""
         return (self.SEPARATOR.join(args) + "\n").encode()
-
-    async def __connect(self):
-        """Start a connection with the server."""
-        return await asyncio.open_connection(*self.ADDRESS)
-
-    async def __close(self, writer):
-        """Close the connection to the server."""
-        writer.close()
-        await writer.wait_closed()
 
     def set_mynt_id(self, mynt_id: str):
         """Set the Mynt ID that the client will attempt to connect to. Note that this
         function must be called before functions like send and receive are use!"""
         self.mynt_id = mynt_id
 
-    async def send(self, data: str):
+    @connect_and_close
+    async def send(self, _, writer, data: str):
         """A function for sending data to the Mynt server."""
-        if self.mynt_id is None:
-            return
-
-        _, writer = await self.__connect()
-
         writer.write(self.__join(self.uid, self.mynt_id, data))
-        await writer.drain()
 
-        await self.__close(writer)
-
-    async def receive(self):
+    @connect_and_close
+    async def receive(self, reader, writer):
         """A function for receiving data from the Mynt server."""
-        if self.mynt_id is None:
-            return
-
-        reader, writer = await self.__connect()
-
         writer.write(self.__join(self.uid, self.mynt_id))
-        await writer.drain()
-
-        result = (await reader.read(self.MAX_MESSAGE_SIZE)).decode()
-        await self.__close(writer)
-        return result
+        return (await reader.read(self.MAX_MESSAGE_SIZE)).decode()
 
 
 # try to talk between the two clients (when the server is running)
