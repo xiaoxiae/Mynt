959ffa447d37f7da6e058bb80380026cb49f4290
xiaoxiae <tomas.slama.131@gmail.com>
2020-09-20 11:06:23 +0200

    add set client Mynt ID using a function

--- code/client/client.py
+++ code/client/client.py
@@ -9,11 +9,11 @@ class Client:
     ADDRESS = ("localhost", 9106)  # TODO: IP
     SEPARATOR = " | "
 
-    def __init__(self, mynt_id: str, uid: Optional[str] = None):
-        self.mynt_id = mynt_id
+    def __init__(self, uid: Optional[str] = None):
         self.uid = uid or hex(get_mac())
+        self.mynt_id is None
 
-    def join(self, *args) -> bytes:
+    def __join(self, *args) -> bytes:
         """Create a message by joining parts of it using a separator"""
         return (self.SEPARATOR.join(args) + "\n").encode()
 
@@ -26,20 +26,31 @@ class Client:
         writer.close()
         await writer.wait_closed()
 
+    def set_mynt_id(self, mynt_id: str):
+        """Set the Mynt ID that the client will attempt to connect to. Note that this
+        function must be called before functions like send and receive are use!"""
+        self.mynt_id = mynt_id
+
     async def send(self, data: str):
         """A function for sending data to the Mynt server."""
+        if self.mynt_id is None:
+            return
+
         _, writer = await self.__connect()
 
-        writer.write(self.join(self.uid, self.mynt_id, data))
+        writer.write(self.__join(self.uid, self.mynt_id, data))
         await writer.drain()
 
         await self.__close(writer)
 
     async def receive(self):
         """A function for receiving data from the Mynt server."""
+        if self.mynt_id is None:
+            return
+
         reader, writer = await self.__connect()
 
-        writer.write(self.join(self.uid, self.mynt_id))
+        writer.write(self.__join(self.uid, self.mynt_id))
         await writer.drain()
 
         result = (await reader.read(self.MAX_MESSAGE_SIZE)).decode()
@@ -49,8 +60,11 @@ class Client:
 
 # try to talk between the two clients (when the server is running)
 if __name__ == "__main__":
-    c1 = Client("m1", "c1")
-    c2 = Client("m1", "c2")
+    c1 = Client("c1")
+    c1.set_mynt_id("m1")
+
+    c2 = Client("c2")
+    c2.set_mynt_id("m1")
 
     loop = asyncio.get_event_loop()
 
