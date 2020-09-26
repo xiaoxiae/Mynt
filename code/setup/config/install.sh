b8684bfbbca3f5d457e1ca80cf27c3a924d0c7b5
xiaoxiae <tomas.slama.131@gmail.com>
2020-09-26 11:14:45 +0200

    enable i2c when installing

--- code/setup/config/install.sh
+++ code/setup/config/install.sh
@@ -6,11 +6,13 @@ DEBUG=1
 set -eu
 cd "$(dirname "$0")"
 
+
 # update, install apps
+echo "Updating and installing packages."
 apt-get update -y
 apt-get upgrade -y
 
-apt install git python3-pip -y
+apt-get install git python3-pip -y
 pip3 install \
 	pyyaml \
 	typeguard \
@@ -23,7 +25,9 @@ then
 	apt install vim tmux -y
 fi
 
+
 # set up (u)MTP
+echo "Enabling MTP."
 mv "rc.local" "/etc/rc.local"
 chmod +x "/etc/rc.local"
 
@@ -36,6 +40,20 @@ git clone https://github.com/viveris/uMTP-Responder umtp
 make -C umtp
 sudo mv umtp/umtprd /usr/bin/umtprd
 
+
+# i2c stuff
+echo "Enabling i2c."
+apt-get install i2c-tools -y
+
+echo "i2c-bcm2708" | sudo tee -a /etc/modules
+echo "i2c-dev" | sudo tee -a /etc/modules
+
+echo "dtparam=i2c0=on" | sudo tee -a /boot/config.txt
+echo "dtparam=i2c1=on" | sudo tee -a /boot/config.txt
+echo "dtparam=i2c_arm=on" | sudo tee -a /boot/config.txt
+
+
+# clean-up
 echo "Cleaning up."
 rm ~/install.sh
 rm -r umtp
